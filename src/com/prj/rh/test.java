fecd92150b47150109a015fec3fc9a25345118ac
renhao <m15991769527@163.com>
2017-12-07 16:41:44 +0800

    测试专用

---
+++ src/com/prj/rh/test.java
@@ -0,0 +1,10 @@
+package com.prj.rh;
+
+/**
+ * Created by suneee on 2017/12/7.
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Say Hello world");
+    }
+}
